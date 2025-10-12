ad6a7277b2af5fc7d6f440bd7980d6097d29d78a
Rafa <rafacrimson2@gmail.com>
2025-10-13 05:54:50 +0800

    P6 Pengantar PHP

---
+++ P6/contoh.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Di Html</title>
+</head>
+<body>
+    <h1>PHP Di Html</h1>
+    <p>
+        <?php
+            echo "Hello World";
+        ?>
+    </p>
+    <h2>Dapat digunakan langsung di HTML</h2>
+</body>
+</html>
