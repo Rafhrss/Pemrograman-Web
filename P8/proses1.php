e43f38b7e7fe33cbc604c04661905badcd2120df
Rafa <rafacrimson2@gmail.com>
2025-11-11 21:50:29 +0800

    Remove 03_post_get

--- P8/proses1.php
+++
@@ -1,6 +0,0 @@
-<?php
-$bilangan1 = $_POST['bil1'];
-$bilangan2 = $_POST['bil2'];
-echo '<p>Anda telah memasukkan bilangan pertama = '.$bilangan1.'<p/>';
-print'<p>Anda telah memasukkan bilangan pertama = '.$bilangan2.'<p/>';
-?>
